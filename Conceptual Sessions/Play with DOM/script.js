defa4266f24186584676a8ec129178353e67d668
MuhammadTanvirHasan98 <muhammadtanvirhasan98@gmail.com>
2024-04-13 22:58:53 +0600

    added conceptual sessions

---
+++ Conceptual Sessions/Play with DOM/script.js
@@ -0,0 +1,34 @@
+
+
+// console.log("Hello World")
+
+
+const li = document.getElementsByTagName("li");
+// console.log(ul);
+for(const l of li){
+   console.log(l);
+}
+
+//  const li = document.createElement("li");
+//  li.innerText = 'HTML';
+
+//  ul.appendChild(li);
+
+//  const div= document.querySelector('div');
+//  console.log(div);
+//  div.classList.add('first','second','third');
+
+// //  h3.classList.remove('first')
+
+// const h3= document.querySelector('h3');
+// console.log(h3);
+
+//  const h2 = document.createElement('h2');
+//  h2.innerText = 'Hello My Dear Brother'
+//  console.log(h2);
+//  div.insertBefore(h2,h3);
+
+
+
+//  const ul = document.querySelector("ul");
+//  const second =
